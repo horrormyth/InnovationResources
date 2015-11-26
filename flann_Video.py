4aa591687a034bb25719a2cd9a0c702f9ca521c9
horrormyth <horrormyth@gmail.com>
2015-11-26 18:11:47 +0200

    Flann with best match with result of match or not, No Video Display

---
+++ flann_Video.py
@@ -0,0 +1,60 @@
+__author__ = 'devndraghimire'
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+MIN_MATCH_COUNT =20
+import imutils
+
+#FLANN PARAMS
+FLANN_INDEX_KDTREE = 0
+index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+search_params = dict(checks=50)   # or pass empty dictionary
+
+flann = cv2.FlannBasedMatcher(index_params,search_params)
+
+surf = cv2.xfeatures2d.SURF_create()
+cap = cv2.VideoCapture(0)
+
+img1 = cv2.imread('Karhu.png')
+kp1,des1 = surf.detectAndCompute(img1,None)
+
+while True:
+    ret,img2 =cap.read()
+    kp2,des2 = surf.detectAndCompute(img2,None)
+    # print len(kp2)
+
+    #flann Matcher
+    matches = flann.knnMatch(des1,des2, k=2)
+
+    good_Matches = []
+    for m,n in matches:
+        if m.distance <0.75*n.distance:
+            good_Matches.append(m)
+    #Compare matches if lengths are good and find the location
+
+    if len(good_Matches)>MIN_MATCH_COUNT:
+        src_pts = np.float32([kp1[m.queryIdx].pt for m in good_Matches]).reshape(-1,1,2)
+        dst_pts = np.float32([kp2[m.queryIdx].pt for m in good_Matches]).reshape(-1,1,2)
+
+        M, mask = cv2.findHomography(src_pts,dst_pts,cv2.RANSAC,5,0)
+        matches_Mask = mask.ravel().tolist()
+        # print(img1.shape)
+        h,w = img1.shape[:2]
+        pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+        dst =cv2.perspectiveTransform(pts,M)
+
+        result = cv2.polylines(img2,[np.int32(dst)],True,255,3,cv2.LINE_AA)
+        print ('MATCHES FOUND!!!!')
+
+    else:
+        print 'Not enough matches found - %d/%d' % (len(good_Matches),MIN_MATCH_COUNT)
+        matches_Mask = None
+
+
+    # cv2.imshow('camera',result)
+    # draw_params = dict(matchColor = (0,255,0),
+    #                    singlePointColor = (255,0,0),
+    #                    matchesMask = good_Matches,
+    #                    flags =0)
+    # img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,matches,None,**draw_params)
+    # plt.imshow(img3),plt.show()
