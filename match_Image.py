5369801681aeedbf17667046796f939ae593d7c4
horrormyth <horrormyth@gmail.com>
2015-12-01 19:00:10 +0200

    Some formatting and comment is done!!

--- match_Image.py
+++ match_Image.py
@@ -2,46 +2,41 @@ __author__ = 'devndraghimire'
 
 import numpy as np
 import cv2
-from matplotlib import pyplot as plt
-import imutils
 
-rad =2
+
 orb = cv2.ORB_create()
-capture = cv2.VideoCapture(0) #video capture
-#stat image processing
-def_Image = cv2.imread('Karhu.png')
+#video capture
+capture = cv2.VideoCapture(0)
+#start image processing
+def_Image = cv2.imread('images.jpeg')
 changeto_Gray = cv2.cvtColor(def_Image,cv2.COLOR_BGR2GRAY)
 kp_image,des_image = orb.detectAndCompute(changeto_Gray,None)
-# print len(kp_image)
-# print len(des_image)
+
 
 #For video
 while True:
+    # Frame retrieving
     ret, vid_Image = capture.read()
     gray_Vid = cv2.cvtColor(vid_Image,cv2.COLOR_BGR2GRAY)
-    kp_Vid, des_Vid = orb.detectAndCompute(gray_Vid,None)
-    # print len(kp_Vid)
-    for keypoints in kp_Vid:
-        x=int(keypoints.pt[0])
-        y=int(keypoints.pt[0])
-        # cv2.circle(gray_Vid,(x,y),rad,(0,0,255))
 
-    #Find Matches
+    # Keypoint and Descriptors
+    kp_Vid, des_Vid = orb.detectAndCompute(gray_Vid,None)
+
+    # matches by bruteforce matcher using knn algorithm
     bfmatcher = cv2.BFMatcher()
     allmatch = bfmatcher.knnMatch(des_image,des_Vid,k=2)
-    # print allmatch
-    # Test the matches
+
+    # put the matches in Array having the match distance satisfied by 75 %
     best_Match =[]
     for matcha, matchb in allmatch:
         if matcha.distance < 0.75 * matchb.distance:
             best_Match.append([matcha])
-            # print len(best_Match)# length of the match
 
     # Traces the matches only, not the best match -- tracing best matches
 
     # TO-DO
 
-    finalimage= cv2.drawMatchesKnn(changeto_Gray,kp_image,gray_Vid,kp_Vid,allmatch,None,flags=2)
+    finalimage= cv2.drawMatchesKnn(def_Image,kp_image,gray_Vid,kp_Vid,allmatch,None,flags=2)
     cv2.imshow('win',finalimage)
 
 
